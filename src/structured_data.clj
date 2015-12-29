6bc4de7eb9416d52eeb7c810ada5f701033fe5d4
Dhruv Pancholi <dhruv.pancholi@flipkart.com>
2015-12-29 13:40:28 +0530

    Before Sets

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,5 +1,20 @@
 (ns structured-data)
 
+(def china {:name "China Miéville", :birth-year 1972})
+(def octavia {:name "Octavia E. Butler"
+              :birth-year 1947
+              :death-year 2006})
+(def friedman {:name "Daniel Friedman" :birth-year 1944})
+(def felleisen {:name "Matthias Felleisen"})
+
+(def cities {:title "The City and the City" :authors [china]})
+(def wild-seed {:title "Wild Seed", :authors [octavia]})
+(def embassytown {:title "Embassytown", :authors [china]})
+(def little-schemer {:title "The Little Schemer"
+                     :authors [friedman, felleisen]})
+
+(println "Loading structured-data")
+
 (defn do-a-thing-orig [x]
   (Math/pow (+ x x) (+ x x)))
 
@@ -23,6 +38,9 @@
     :else (let [[v1 v2 v3] v] (+ v1 v3))
     ))
 
+(defn sum-pairs [[x1 y1] [x2 y2]]
+  [(+ x1 x2) (+ y1 y2)])
+
 (defn point [x y]
   [x y])
 
@@ -30,52 +48,79 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1))
+  )
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1))
+  )
 
 (defn square? [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (cond
+      (= (- x2 x1) (- y2 y1)) true
+      :else false)
+    ))
 
 (defn area [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (* (- x2 x1) (- y2 y1))
+    ))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+        [x3 y3] point]
+    (cond
+      (and (<= x1 x3 x2) (<= y1 y3 y2)) true
+      :else false)
+    ))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[x1 y1] [x2 y2]] outer
+        [[x3 y3] [x4 y4]] inner]
+    (cond
+      (and (<= x1 x3 x4 x2) (<= y1 y3 y4 y2)) true
+      :else false)
+    ))
 
 (defn title-length [book]
-  :-)
+  (count (get book :title)))
 
 (defn author-count [book]
-  :-)
+  (count (get book :authors)))
 
 (defn multiple-authors? [book]
-  :-)
+  (> (count (get book :authors)) 1))
 
 (defn add-author [book new-author]
-  :-)
+  (assoc book :authors (conj (get book :authors) new-author))
+  )
 
 (defn alive? [author]
-  :-)
+  (not (contains? author :death-year))
+  )
 
 (defn element-lengths [collection]
-  :-)
+  (map count collection))
 
 (defn second-elements [collection]
-  :-)
+  (map second collection))
 
 (defn titles [books]
-  :-)
+  (map :title books))
+
+; since a list of maps, and this from every map
+; Can be extracted the given key
+; (println (:name (get (:authors cities) 0)))
+; (println (author-names cities))
 
 (defn monotonic? [a-seq]
-  :-)
+  (or (apply <= a-seq) (apply >= a-seq)))
 
 (defn stars [n]
-  :-)
+  (apply str (repeat n "*")))
 
 (defn toggle [a-set elem]
   :-)
@@ -91,6 +136,18 @@
 
 (defn authors [books]
   :-)
+
+(def books [cities, wild-seed, embassytown, little-schemer])
+
+(def s (seq [1 2 3]))
+; (println (seq {:a 42 :b "foo" :c ["ur" "dad"]}))
+; (println s)
+; (println (first s))
+; (println (rest s))
+; (println (cons 0 s))
+
+(defn author-names [book]
+  (map :name (:authors book)))
 
 (defn all-author-names [books]
   :-)
--- test/structured_data_test.clj
+++ test/structured_data_test.clj
@@ -1,2 +1,123 @@
 (ns structured-data-test
-  (:use iloveponies.tests.structured-data))
+  (:use midje.sweet
+        structured-data))
+
+(facts "height" {:exercise 1
+					:points 1}
+	(height (rectangle [1 1] [5 1])) => 0
+	(height (rectangle [1 1] [5 5])) => 4
+	(height (rectangle [0 0] [2 3])) => 3)
+
+(facts "width" {:exercise 2
+				:points 1}
+	(width (rectangle [1 1] [5 1]))  => 4
+	(width (rectangle [1 1] [1 1]))  => 0
+	(width (rectangle [3 1] [10 4])) => 7)
+
+(facts "square" {:exercise 3
+				:points 1}
+	(square? (rectangle [1 1] [2 2])) => true
+	(square? (rectangle [1 1] [2 3])) => false
+	(square? (rectangle [1 1] [1 1])) => true
+	(square? (rectangle [3 2] [1 0])) => true
+	(square? (rectangle [3 2] [1 1])) => false)
+
+(facts "area" {:exercise 4
+				:points 1}
+	(area (rectangle [1 1] [5 1]))  => 0
+	(area (rectangle [0 0] [1 1]))  => 1
+	(area (rectangle [0 0] [4 3]))  => 12
+	(area (rectangle [3 1] [10 4])) => 21)
+
+(facts "contains-point?" {:exercise 5
+				:points 1}
+	(contains-point? (rectangle [0 0] [2 2])
+                 (point 1 1))            => true
+	(contains-point? (rectangle [0 0] [2 2])
+	                 (point 2 1))            => true
+	(contains-point? (rectangle [0 0] [2 2])
+	                 (point -3 1))           => false
+	(contains-point? (rectangle [0 0] [2 2])
+	                 (point 1 3))            => false
+	(contains-point? (rectangle [1 1] [2 2])
+	                 (point 1 1))            => true
+	(contains-point? (rectangle [1 1] [1 1])
+	                 (point 1 1))            => true)
+
+(facts "contains-rectangle?" {:exercise 6
+				:points 1}
+	(contains-rectangle? (rectangle [0 0] [3 3])
+	                     (rectangle [1 1] [2 2])) => true
+	(contains-rectangle? (rectangle [0 0] [2 2])
+	                     (rectangle [1 1] [3 3])) => false
+	(contains-rectangle? (rectangle [0 0] [1 1])
+	                     (rectangle [0 0] [1 1])) => true
+	(contains-rectangle? (rectangle [0 0] [1 1])
+	                     (rectangle [1 1] [2 2])) => false)
+
+(facts "title-length" {:exercise 7
+				:points 1}
+	(title-length cities)         => 21
+	(title-length wild-seed)      => 9
+	(title-length little-schemer) => 18)
+
+(facts "author-count" {:exercise 8
+				:points 1}
+	(author-count cities)         => 1
+	(author-count wild-seed)      => 1
+	(author-count little-schemer) => 2)
+
+(facts "multiple-authors?" {:exercise 9
+				:points 1}
+	(multiple-authors? cities)         => false
+	(multiple-authors? wild-seed)      => false
+	(multiple-authors? little-schemer) => true)
+
+(facts "add-author" {:exercise 10
+				:points 1}
+	(add-author little-schemer {:name "Gerald J. Sussman"})
+		=> {:title "The Little Schemer"
+		   :authors [{:birth-year 1944, :name "Daniel Friedman"}
+		             {:name "Matthias Felleisen"}
+		             {:name "Gerald J. Sussman"}]}
+	(add-author {:authors [{:name "Juhana"}]} {:name "Jani"})
+		=> {:authors [{:name "Juhana"} {:name "Jani"}]})
+
+(facts "alive?" {:exercise 11
+				:points 1}
+	(alive? china)   => true
+	(alive? octavia) => false)
+
+(facts "element-lengths" {:exercise 12
+				:points 1}
+	(element-lengths ["foo" "bar" "" "quux"])	=> [3 3 0 4]
+	(element-lengths ["x" [:a :b :c] {:y 42}])	=> [1 3 1])
+
+(facts "second-elements" {:exercise 13
+				:points 1}
+	(second-elements [[1 2] [2 3] [3 4]])	=> [2 3 4]
+	(second-elements [[1 2 3 4] [1] ["a" "s" "d" "f"]])	=> [2 nil "s"])
+
+(facts "titles" {:exercise 14
+				:points 1}
+	(titles [cities]) 	=> ["The City and the City"]
+	(titles books) 		=> ["The City and the City" "Wild Seed" "Embassytown" "The Little Schemer"])
+
+(facts "author-names" {:exercise 15
+				:points 1}
+	(author-names cities)		=> ["China Miéville"]
+	(author-names little-schemer)	=> ["Daniel Friedman" "Matthias Felleisen"])
+
+(facts "stars" {:exercise 16
+				:points 1}
+	(stars 1) => "*"
+	(stars 7) => "*******"
+	(stars 3) => "***")
+
+(facts "monotonic?" {:exercise 16
+				:points 1}
+	(monotonic? [1 2 3])     => true
+	(monotonic? [0 1 10 11]) => true
+	(monotonic? [3 2 0 -3])  => true
+	(monotonic? [3 2 2])     => true;    Not strictly monotonic
+	(monotonic? [1 2 1 0])   => false)
